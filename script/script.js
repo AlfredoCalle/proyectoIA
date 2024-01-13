a5b4aa00c04bdf06a3abda598877fb5eadd9b832
PaulGudino <pgudino@espol.edu.ec>
2024-01-13 18:08:07 -0500

    feat: change how to capture the image

--- script/script.js
+++ script/script.js
@@ -34,7 +34,7 @@ async function detectWebcam() {
 
         if (isMobile) {
             constraints.video.facingMode = { exact: "environment" };
-        } 
+        }
 
         const stream = await navigator.mediaDevices.getUserMedia(constraints);
         handleSuccess(stream);
@@ -44,7 +44,6 @@ async function detectWebcam() {
         document.getElementById("mensaje").innerHTML = "No se puede acceder a la cámara";
     }
 }
-
 
 function handleSuccess(stream) {
     window.stream = stream;
@@ -57,11 +56,18 @@ async function classifyAndShowImage() {
 
     const imageCapture = new ImageCapture(window.stream.getVideoTracks()[0]);
     const img = await imageCapture.grabFrame();
-    const blob = await createImageBlob(img);
-    capturedImage.src = URL.createObjectURL(blob);
+
+    const canvas = document.createElement('canvas');
+    canvas.width = 640;
+    canvas.height = 480;
+    const context = canvas.getContext('2d');
+    context.drawImage(img, 0, 0, 640, 480);
+
+    capturedImage.src = canvas.toDataURL(); // Muestra la imagen capturada en el elemento 'capturedImage'
+
     imageContainer.style.display = "block";
     video.style.display = "none";
-    
+
     btnClasificar.style.display = "none";
     btnNuevo.style.display = "block";
 }
@@ -84,8 +90,13 @@ async function classifyFrame() {
 async function classify() {
     const imageCapture = new ImageCapture(window.stream.getVideoTracks()[0]);
     const img = await imageCapture.grabFrame();
-    const context = canvas.getContext("2d");
+
+    const canvas = document.createElement('canvas');
+    canvas.width = 640;
+    canvas.height = 480;
+    const context = canvas.getContext('2d');
     context.drawImage(img, 0, 0, 640, 480);
+
     const tensor = tf.browser
         .fromPixels(canvas)
         .resizeBilinear([224, 224])
@@ -97,41 +108,25 @@ async function classify() {
 }
 
 function updateResult(predictions) {
-    // const labels = ["Bailarina", "Deportivos", "Formales", "Mocasines", "Plataformas", "Sandalias", "Tacones"];
     const labels = ["Bailarina", "Mocasines", "Formales", "Suecos", "Deportivos"];
-    var dic_resultado = {}; 
-    // Sumar todas las predicciones para obtener el total
+    var dic_resultado = {};
     const totalPredictions = predictions.reduce((acc, val) => acc + val, 0);
 
-    // Calcular y mostrar el porcentaje para cada clase
     for (let i = 0; i < predictions.length; i++) {
-        const resultPercentage = ((predictions[i] / totalPredictions) * 100);
+        const resultPercentage = (predictions[i] / totalPredictions) * 100;
         console.log(`Etiqueta: ${labels[i]}, Porcentaje: ${resultPercentage}%`);
         dic_resultado[labels[i]] = resultPercentage;
     }
 
-    // Obtener la clase con la mayor predicción
-    const maxPredictionIndex = predictions.indexOf(Math.max(...predictions));
-    const resultLabel = labels[maxPredictionIndex];
-    
-    //Ordenar diccionario
-    // const entries = Object.entries(dic_resultado);
-    // entries.sort((a, b) => b[1] - a[1]);
     const sortedObject = Object.entries(dic_resultado).sort((a, b) => b[1] - a[1]);
     const topThreeLabels = sortedObject.slice(0, 3);
     const resultElement = document.getElementById("resultado");
     resultElement.innerHTML = "";
     for (let i = 0; i < topThreeLabels.length; i++) {
         const [key, value] = topThreeLabels[i];
-        // Utilizar toFixed(4) para mostrar el valor con 4 decimales
         resultElement.innerHTML += `${key}: ${value.toFixed(2)}%<br>`;
-    }    
-    // Mostrar la etiqueta de la clase con mayor predicción
-    // document.getElementById("resultado").innerText = resultLabel;
+    }
 }
-
-
-
 
 function createImageBlob(image) {
     return new Promise((resolve) => {
